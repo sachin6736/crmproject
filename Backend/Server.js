3f16bc3d7d1841750a74d1ed309b6b92bc1d8005
Sachin A <sachinpradeepan27@gmail.com>
2025-03-29 12:06:45 +0530

    initial commit - frontend and backend

---
+++ Backend/Server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const port = process.env.port || 5000
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('mongodb connected'))
+  .catch(err => console.log('error occured', err))
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`)
+})
