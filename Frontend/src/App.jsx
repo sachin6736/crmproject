cac681c487f90a07164242535b1d6d8dac17923f
Ananya Jan <ananyajan2000@gmail.com>
2025-03-31 22:37:32 +0530

    Added Sidebar and Header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Userfrom from './components/Userfrom'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,8 @@ function App() {
     {/* <h1 className="text-3xl font-bold underline text-slate-600">
       Hello world!
     </h1> */}
-    <Userfrom/>
+    {/* <Userfrom/> */}
+    <Home/>
     </>
   )
 }
---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Home as HomeIcon,Users,Briefcase,LineChart,Headset,Megaphone,LucideShoppingCart,PenTool,User} from "lucide-react"
+
+function Home() {
+  return (
+    <div className='w-screen h-screen bg-white flex'>
+     <div className="w-20 h-screen bg-[#002775] fixed left-0 top-0 border-r-[2px] border-r-white flex flex-col items-center pt-3 space-y-3 overflow-auto">
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+           <HomeIcon className='h-6 w-6 text-white'/>
+          </div>
+          <span className="text-white text-[10px] font-bold">Home</span>
+        </div>
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+            <Users className='h-6 w-6 text-white'/>
+          </div>
+          <span className="text-white text-[10px] font-bold">Contacts</span>
+        </div>
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+           <Briefcase className='h-6 w-6 text-white'/>
+          </div>
+          <span className="text-white text-[10px] font-bold">Accounts</span>
+        </div>
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+          <LineChart className='h-6 w-6 text-white' />
+          </div>
+          <span className="text-white text-[10px] font-bold">Sales</span>
+        </div>
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+            <Headset className='h-6 w-6 text-white'/>
+          </div>
+          <span className="text-white text-[10px] font-bold">Service</span>
+        </div>
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+            <Megaphone className='h-6 w-6 text-white' />
+          </div>
+          <span className="text-white text-[10px] font-bold">Outreach</span>
+        </div>
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+            <LucideShoppingCart className='h-6 w-6 text-white'/>
+          </div>
+          <span className="text-white text-[10px] font-bold">Commerce</span>
+        </div>
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+            <PenTool className='h-6 w-6 text-white' />
+          </div>
+          <span className="text-white text-[10px] pl-4 font-bold">Generative Canvas</span>
+        </div>
+        <div className="flex flex-col items-center space-y-1">
+          <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
+            <User className='h-6 w-6 text-white'/>
+          </div>
+          <span className="text-white text-[10px] font-bold">Your Account</span>
+        </div>
+     </div>
+     <div className="flex-1 h-screen ml-20">
+        <div className="w-full h-12 bg-[#066afe]"></div>
+        <div className="p-4">Main Content</div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
