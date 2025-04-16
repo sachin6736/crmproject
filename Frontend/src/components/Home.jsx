5462985ffea16c0494d963a1869d1dacf992d844
Ananya Jan <ananyajan2000@gmail.com>
2025-04-16 23:25:49 +0530

    Added dashboard route

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -60,7 +60,7 @@ function Home() {
           </div>
           <span className="text-white text-[10px] font-bold">Service</span>
         </div>
-        <div className="flex flex-col items-center space-y-1">
+        <div className="flex flex-col items-center space-y-1" onClick={()=> navigate('/home/dashboard')}>
           <div className='w-12 h-12 bg-[#002775] rounded-md border border-[#002775] hover:border-white transition duration-300 flex items-center justify-center'>
             <Megaphone className='h-6 w-6 text-white' />
           </div>
