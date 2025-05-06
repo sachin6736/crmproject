36b16fe3691bb4b4881cb3b45e19077c91862874
Ananya Jan <ananyajan2000@gmail.com>
2025-05-06 14:02:58 +0530

    Changes

--- Frontend/src/components/Leads.jsx
+++ Frontend/src/components/Leads.jsx
@@ -291,7 +291,7 @@ const LeadTableHeader = () => {
             </div>
           </div>
 
-          <div className="mt-4 bg-white rounded-md shadow-md overflow-x-auto flex-grow relative">
+          <div className="mt-4 bg-white rounded-md shadow-md overflow-x-auto h-[720px] flex-grow relative">
             {loadingLeads ? (
               <div className="flex justify-center items-center py-8">
                 <FullPageLoader />
