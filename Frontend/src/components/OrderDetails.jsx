79b261a0cd3f20cda11865e4a3ed71115ec49dae
Ananya Jan <ananyajan2000@gmail.com>
2025-07-07 14:09:36 +0530

    changes

--- Frontend/src/components/OrderDetails.jsx
+++ Frontend/src/components/OrderDetails.jsx
@@ -2564,7 +2564,7 @@ const [selectedVendorId, setSelectedVendorId] = useState(null)
                 Edit Order Details
               </button>
             )}
-           {["procurement", "admin"].includes(user?.role) && order?.status === "Intransit" && (
+         {["procurement", "admin"].includes(user?.role) && (order?.status === "Intransit" || order?.status === "Delivered") && (
   <button
     onClick={handleShipmentDelivered}
     className={`w-full px-4 py-2 text-white rounded-md transition-colors text-sm sm:text-base ${
@@ -2592,6 +2592,7 @@ const [selectedVendorId, setSelectedVendorId] = useState(null)
     )}
   </button>
 )}
+
             <button
               onClick={handleExportToExcel}
               className="w-full px-4 py-2 bg-blue-600 dark:bg-blue-500 text-white rounded-md hover:bg-blue-700 dark:hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors text-sm sm:text-base"
