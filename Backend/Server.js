0d4ab08fa67cdeccb0adb721d7a179bf88e4dfc4
Sachin A <sachinpradeepan27@gmail.com>
2025-08-29 13:57:28 +0530

    patchrequest

--- Backend/Server.js
+++ Backend/Server.js
@@ -36,7 +36,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Explicitly allow methods
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Explicitly allow methods
   allowedHeaders: ['Content-Type', 'Authorization'] // Allow necessary headers
 }));
 
